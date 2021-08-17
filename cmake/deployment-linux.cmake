7983f6984aee558b2898b8ed19b11f2460328e6c
Moody <76251897+moodyhunter@users.noreply.github.com>
2021-08-17 15:45:43 +0800

    + CMake files cleanup

--- cmake/deployment-linux.cmake
+++ cmake/deployment-linux.cmake
@@ -3,23 +3,17 @@ install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/assets/qv2ray.metainfo.xml DESTINATION
 install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/assets/qv2ray.desktop DESTINATION share/applications)
 install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/assets/icons/qv2ray.svg DESTINATION share/icons/hicolor/scalable/apps)
 
-if(QV2RAY_QV2RAYBASE_PROVIDER STREQUAL "module")
-    if(BUILD_SHARED_LIBS)
+if(BUILD_SHARED_LIBS)
+    if(QV2RAY_QV2RAYBASE_PROVIDER STREQUAL "module")
         install(TARGETS Qv2rayBase)
     endif()
-endif()
 
-if(QV2RAY_QNODEEDITOR_PROVIDER STREQUAL "module")
-    if(BUILD_SHARED_LIBS)
-        if(QV2RAY_ROUTE_EDITOR)
-            install(TARGETS nodes)
-        endif()
+    if(QV2RAY_QNODEEDITOR_PROVIDER STREQUAL "module" AND QV2RAY_ROUTE_EDITOR)
+        install(TARGETS nodes)
     endif()
 endif()
 
-set(QV2RAY_ICON_DIMENSIONS 16 22 32 48 64 128 256 512 1024)
-
-foreach(d ${QV2RAY_ICON_DIMENSIONS})
+foreach(d 16 22 32 48 64 128 256 512 1024)
     install(FILES assets/icons/qv2ray.${d}.png
         DESTINATION share/icons/hicolor/${d}x${d}/apps
         RENAME qv2ray.png)
